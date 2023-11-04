3878554169c8f7d69671152b8fbd4cd43731bc41
Shinnzy <temidireadetayo@gmail.com>
2023-11-04 06:01:20 -0700

    home page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'global-blue': '#0C2461',
+        'global-green' : '#1DD1A1',
+        'primary-blue': '#1255DD'
+      },
       scrollPadding: {
         '1.5': '0.3rem',
       },
