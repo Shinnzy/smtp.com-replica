741d30d70617db6473002e23200e0bfdbfcab765
Shinnzy <temidireadetayo@gmail.com>
2023-12-21 09:15:45 -0800

    front page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'global-blue': '#0C2461',
+        'global-white': '#F1F0EE',
         'global-green' : '#1DD1A1',
-        'primary-blue': '#1255DD'
+        'primary-blue': '#1255DD',
+        'global-brown' :'#E2D9C8',
+        'primary-brown' : '#30261C',
+
       },
       scrollPadding: {
         '1.5': '0.3rem',
       },
+      backgroundImage: {
+        'hero': "url('https://ik.imagekit.io/2akrvp545/pexels-chevanon-photography-302899.jpg?updatedAt=1701004125647')",
+        'sub' : "url('https://ik.imagekit.io/2akrvp545/coffeebeans.png?updatedAt=1702813526853')"gitgit
+      }
     },
   },
   plugins: [],
